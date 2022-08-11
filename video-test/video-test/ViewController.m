fe5a817aa0e685fd487413dc7cab009dcefb5eb5
bobo008 <1030831926@qq.com>
2022-08-11 17:12:13 +0800

    video

--- video-test/video-test/ViewController.m
+++ video-test/video-test/ViewController.m
@@ -7,6 +7,8 @@
 
 #import "ViewController.h"
 
+#import "THBVideoTestVC.h"
+
 @interface ViewController ()
 
 @end
@@ -16,6 +18,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+
+- (IBAction)onBtn:(id)sender {
+    THBVideoTestVC *vc = [[THBVideoTestVC alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
 
 
---
+++ video-test/video-test/video/THBVideoTestVC.h
@@ -0,0 +1,16 @@
+//
+//  THBVideoTestVC.h
+//  video-test
+//
+//  Created by tanghongbo on 2022/8/11.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface THBVideoTestVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
